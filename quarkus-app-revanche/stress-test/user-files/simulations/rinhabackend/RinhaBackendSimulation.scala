00e9a20f6f66c806d68fa73fb6e0452fa7dc0500
John OHara <johara@redhat.com>
2023-09-07 09:51:47 +0100

    Run single end-point closed model

--- quarkus-app-revanche/stress-test/user-files/simulations/rinhabackend/RinhaBackendSimulation.scala
+++ quarkus-app-revanche/stress-test/user-files/simulations/rinhabackend/RinhaBackendSimulation.scala
@@ -31,7 +31,7 @@ class RinhaBackendSimulation
         header("Location").saveAs("location")
       }
     )
-    .pause(1.milliseconds, 30.milliseconds)
+//    .pause(1.milliseconds, 30.milliseconds)
     .doIf(session => session.contains("location")) {
       exec(
         http("consulta")
@@ -57,21 +57,14 @@ class RinhaBackendSimulation
 
   setUp(
     criacaoEConsultaPessoas.inject(
-      constantUsersPerSec(2).during(10.seconds), // warm up
-      constantUsersPerSec(5).during(15.seconds).randomized, // are you ready?
-      
-      rampUsersPerSec(6).to(600).during(3.minutes) // lezzz go!!!
-    ),
-    buscaPessoas.inject(
-      constantUsersPerSec(2).during(25.seconds), // warm up
-
-      rampUsersPerSec(6).to(100).during(3.minutes) // lezzz go!!!
-    ),
-    buscaInvalidaPessoas.inject(
-      constantUsersPerSec(2).during(25.seconds), // warm up
-
-      rampUsersPerSec(6).to(40).during(3.minutes) // lezzz go!!!
-)
+      constantConcurrentUsers(1).during(3.minutes), // warm up
+    )
+//    ,buscaPessoas.inject(
+//      constantConcurrentUsers(4).during(3.minutes) // lezzz go!!!
+//    )
+//    ,buscaInvalidaPessoas.inject(
+//      constantConcurrentUsers(4).during(3.minutes) // lezzz go!!!
+//    )
     
   ).protocols(httpProtocol)
 }
